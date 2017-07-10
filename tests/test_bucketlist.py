4ee22232294e4465069079906e2215f001c08231
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-11 00:36:09 +0300

    Restructure TestCases

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -10,8 +10,10 @@ class BucketlistTestCases(TestCase):
     def setUp(self):
         self.app = create_application(config_name="testing")
         self.client = self.app.test_client
-        self.bucketlist1 = {"fullname": "Travel Manenos"}
-        self.bucketlist2 = {"fullname": "Draw caricatures"}
+        self.bucketlist1 = {"name": "Travel Manenos"}
+        self.bucketlist2 = {"name": "Draw caricatures"}
+        self.bucketlist3 = {"name": "Career Things"}
+        self.bucketlist4 = {"name": "2018"}
 
         # binds app to current context
         with self.app.app_context():
@@ -19,36 +21,64 @@ class BucketlistTestCases(TestCase):
             database.create_all()
 
     def test_bucketlist_creation(self):
-        response = self.client().post('/create-bucketlist/', data=self.bucketlist1)
-        self.assertEqual(response.status_code, 201)
-        self.assertIn('Travel Manenos', str(response.data))
+        response = self.client().post('/v1/api/bucketlists/', data=json.dumps(self.bucketlist1),
+                                      headers={'Content-Type': 'application/json'})
 
-    def test_api_fetch_all_bucketlist(self):
+        data = json.loads(response.data.decode())
+
+
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('Travel Manenos', data['name'], "Bucketlist not created")
+
+    def test_api_fetch_all_bucketlists(self):
         """ Test API can fetch all bucketlists GET request """
 
-        response = self.client().post('/create-bucketlist/', data=self.bucketlist1)
+        response = self.client().post('/v1/api/bucketlists/', data=json.dumps(self.bucketlist2),
+                                      headers={'Content-Type': 'application/json'})
+
         self.assertEqual(response.status_code, 201)
 
-        response2 = self.client().post('/create-bucketlist/', data=self.bucketlist2)
+        response2 = self.client().post('/v1/api/bucketlists/', data=json.dumps(self.bucketlist3),
+                                       headers={'Content-Type': 'application/json'})
+
         self.assertEqual(response2.status_code, 201)
 
-        get_response = self.client().get('/bucketlists/')
+        get_response = self.client().get('/v1/api/bucketlists/')
         self.assertEqual(get_response.status_code, 200)
 
-        self.assertIn('Travel Manenos', str(get_response))
-        self.assertIn('Draw caricatures', str(get_response))
+        data = json.loads(response.data.decode('utf-8'))
+
+        self.assertIn('Draw caricatures', data['name'])
+
 
     def test_api_can_get_bucketlist_by_id(self):
         """ Test API can get a specific bucketlist by id """
 
-        response = self.client().post('/create-bucketlist', data=self.bucketlist1)
+        response = self.client().post('/v1/api/bucketlists/', data=json.dumps(self.bucketlist4),
+                                      headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 201)
 
         json_response = json.loads(response.data.decode('utf-8').replace("'", "\""))
 
-        result = self.client().get('/bucketlists/{}'.format(json_response['id']))
+        result = self.client().get('/v1/api/bucketlists/{}'.format(json_response['id']))
         self.assertEqual(result.status_code, 200)
-        self.assertIn("Travel Manenos", str(result.data))
+
+        data = json.loads(result.data.decode('utf-8'))
+        self.assertIn("2018", data['name'])
+
+
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            database.session.remove()
+            database.drop_all()
+
+
+# Make the tests conveniently executable
+# if __name__ == "__main__":
+#     unittest.main()
 
 
 
