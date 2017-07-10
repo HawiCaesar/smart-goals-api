61e0847cffdde81e4d3bfed9fdd681d6c883c3be
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-10 21:53:47 +0300

    Add bucketlist class

---
+++ bucketlist/models.py
@@ -0,0 +1,40 @@
+from bucketlist import database
+
+
+# class User(database.Model):
+#     pass
+
+
+class Bucketlist(database.Model):
+
+    __tablename__ = 'bucketlists'
+    id = database.Column(database.Integer, primary_key=True)
+    fullname = database.Column(database.String(255))
+    date_created = database.Column(database.Time, default=database.func.current_timestamp())
+    date_modified = database.Column(
+        database.DateTime, default=database.func.current_timestamp(),
+        onupdate=database.func.current_timestamp()
+    )
+
+    def __init__(self, name):
+        self.name = name
+
+    def save(self):
+        database.session.add(self)
+        database.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Bucketlist.query.all()
+
+    def delete(self):
+        database.session.delete(self)
+        database.session.commit()
+
+    def __repr__(self):
+        return "<Bucketlis: {}>".format(self.name)
+
+
+
+# class BucketlistActivity(database.Model):
+#     pass
