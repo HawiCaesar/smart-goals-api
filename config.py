cb5d690fc839080b63801c54707b3535d6f10a75
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-10 21:50:54 +0300

    add testing config

--- config.py
+++ config.py
@@ -1,22 +1,37 @@
 
+import os
+
 # Instance configuartions for the flask application are done here
+
+
 class Config(object):
     # Some Common Configurations
-    DEBUG = True
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
 
 class DevelopmentConfig(Config):
     # Development configurations
 
-    SECRET_KEY = "q38FGSFDsyrefbhj54"
+    DEBUG = True
+
+
+class TestingConfig():
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+
 
 class ProductionConfig():
     # Production configurations
     DEBUG = False
 
 
-
 app_config = {
     "development": DevelopmentConfig(),
+    "testing": TestingConfig(),
     "production": ProductionConfig()
 }
 
