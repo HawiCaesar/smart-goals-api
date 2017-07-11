d0ae4e4fde14dc77bec9b81d3e5b7aab864057d9
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-11 15:08:40 +0300

    Add put and delete resources

--- bucketlist/core_app/views.py
+++ bucketlist/core_app/views.py
@@ -14,28 +14,28 @@ class BucketlistAPI(MethodView):
         data = request.get_json()
 
         bucketlist = Bucketlist(name=data.get('name'))
-        bucketlist.save()
+        bucketlist.save()  # Save bucketlist name
+
         response = jsonify({
-            'id': bucketlist.id,
-            'name': bucketlist.name
+            'status': "Success",
+            'message': "Bucketlist Created"
         })
+
         response.status_code = 201
         return make_response(response)
 
     def get(self, id=None):
-
-        response = {}
 
         if id:
 
             bucketlist = Bucketlist.query.filter_by(id=id).first()
 
             if not bucketlist:
-                response = {
+                response = jsonify({
                     'status': 'Fail',
-                    'message': 'Bucketlist does not exist'
-                }
-                return make_response(jsonify(response)), 404
+                    'message': 'Bucketlist Does Not Exist'
+                })
+                response.status_code = 404
 
             else:
                 response = jsonify({
@@ -43,7 +43,7 @@ class BucketlistAPI(MethodView):
                     'name': bucketlist.name
                 })
 
-                return make_response(response), 200
+                response.status_code = 200
 
         else:
 
@@ -56,6 +56,71 @@ class BucketlistAPI(MethodView):
                     'name': bucketlist.name
                 }
                 results.append(obj)
+
             response = jsonify(results)
             response.status_code = 200
-            return make_response(response)
+
+        return make_response(response)
+
+    def put(self, id=None):
+        if id:
+            bucketlist = Bucketlist.query.filter_by(id=id).first()
+            data = request.get_json()
+
+            if bucketlist:
+                bucketlist.name = data.get("name")
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name
+                })
+
+                response.status_code = 200
+
+            else:
+                response = jsonify({
+                    'status': "Fail",
+                    'message': "Bucketlist name does not exist"
+                })
+
+                response.status_code = 404
+
+        else:
+            response = jsonify({
+                'status': 'Error',
+                'message': 'Please provide an id'
+            })
+
+            response.status_code = 405
+
+        return make_response(response)
+
+    def delete(self, id=None):
+
+        if id:
+            bucketlist = Bucketlist.query.filter_by(id=id).first()
+
+            if bucketlist:
+                bucketlist.delete()
+                response = jsonify({
+                    "status": "Success",
+                    "message": "Bucketlist {} deleted".format(bucketlist.id)
+                })
+
+                response.status_code = 200
+
+            else:
+                response = jsonify({
+                    "status": "Fail",
+                    "message": "Bucketlist does not exist"
+                })
+                response.status_code = 404
+
+        else:
+            response = jsonify({
+                'status': "Fail",
+                'message': "Please provide a bucketlist id"
+            })
+            response.status_code = 405
+
+        return make_response(response)
