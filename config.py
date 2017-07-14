f444b79e35f09476c050db3ca789a7fc49b6417f
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-14 12:49:07 +0300

    Add TestKey used when running tests

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # Some Common Configurations
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.getenv("SECRET")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
@@ -22,6 +22,7 @@ class TestingConfig():
     TESTING = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SECRET_KEY = "This-is-a-T3sT-K3Y"
 
 
 class ProductionConfig():
