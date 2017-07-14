83d5e13506c5bfd3a96c7b41515deeb546ab887e
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-14 12:57:55 +0300

    Add initial test cases for auth module

---
+++ tests/test_auth.py
@@ -0,0 +1,90 @@
+import unittest
+import json
+from bucketlist import create_application, database
+
+class AuthTestCases(unittest.TestCase):
+    """Test case for the authentication blueprint."""
+
+    def setUp(self):
+        """Set up test variables."""
+        self.app = create_application(config_name="testing")
+        # initialize the test client
+        self.client = self.app.test_client
+        # This is the user test json data with a predefined email and password
+        self.user_data = {
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+        self.headers = {'Content-Type': 'application/json'}
+
+        with self.app.app_context():
+            # create all tables
+            database.session.close()
+            database.drop_all()
+            database.create_all()
+
+    def test_can_register_successfully(self):
+        """ Allow user to register """
+
+        response = self.client().post('/v1/api/auth/register', data=json.dumps(self.user_data),
+                                      headers=self.headers)
+
+        result = json.loads(response.data.decode())
+
+        self.assertEqual(result['message'], "User registered successfully.")
+        self.assertEqual(response.status_code, 201)
+
+    def test_registered_user_cannot_register_twice(self):
+        """ A user cannot enter be allowed to register twice """
+
+        response = self.client().post('/v1/api/auth/register', data=json.dumps(self.user_data), headers=self.headers)
+        self.assertEqual(response.status_code, 201)
+
+        response2 = self.client().post('/v1/api/auth/register', data=json.dumps(self.user_data), headers=self.headers)
+        self.assertEqual(response2.status_code, 202)
+
+        result = json.loads(response2.data.decode())
+        self.assertEqual(result['message'], "User already registered. Kindly Login")
+
+    def test_user_login(self):
+        """ Allow the user to login """
+
+        response = self.client().post('/v1/api/auth/register', data=json.dumps(self.user_data), headers=self.headers)
+        self.assertEqual(response.status_code, 201)
+
+        login_response = self.client().post('/v1/api/auth/login', data=json.dumps(self.user_data),
+                                            headers=self.headers)
+
+        result = json.loads(login_response.data.decode())
+
+        self.assertEqual(result['message'], "User has logged in!")
+        self.assertEqual(login_response.status_code, 200)
+        self.assertTrue(result['access_token'])
+
+    def test_non_registered_user_login(self):
+        """Test non registered users cannot login."""
+        # Unregistered User
+
+        non_user = {
+            'email': 'non_user@example.com',
+            'password': 'nope'
+        }
+        # send a POST request to /v1/api/auth/login with the data above
+        response = self.client().post('/v1/api/auth/login', data=json.dumps(non_user), headers=self.headers)
+
+        result = json.loads(response.data.decode())
+
+        # 401(Unauthorized User)
+        #self.assertEqual(response, 401)
+        self.assertEqual(
+            result['message'], "Invalid email or password, Please try again")
+
+    def tearDown(self):
+        """ Teardown all initialized variables """
+        with self.app.app_context():
+            # drop all tables
+            database.session.remove()
+            database.drop_all()
+
+
+
