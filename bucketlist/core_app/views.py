32f3259ab2318f6dccbee3f85242b2d1d8d72051
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-24 22:45:16 +0300

    [Feature 148479819] Add Bucketlist Item Post resource

--- bucketlist/core_app/views.py
+++ bucketlist/core_app/views.py
@@ -1,5 +1,5 @@
 from flask import request, make_response, url_for, jsonify
-from bucketlist.models import Bucketlist, User
+from bucketlist.models import Bucketlist, User, BucketlistItem
 from flask.views import MethodView
 import datetime
 from bucketlist import get_jwt_identity, jwt_required
@@ -15,32 +15,21 @@ class BucketlistAPI(MethodView):
 
         current_user = get_jwt_identity()
 
-        if current_user:
+        bucketlist = Bucketlist(name=data.get('name'), created_by=current_user)
+        bucketlist.save()  # Save bucketlist name
 
-            bucketlist = Bucketlist(name=data.get('name'), created_by=current_user)
-            bucketlist.save()  # Save bucketlist name
+        response = jsonify({
+            'status': "Success",
+            'message': "Bucketlist Created"
+        })
 
-            response = jsonify({
-                'status': "Success",
-                'message': "Bucketlist Created"
-            })
-
-            response.status_code = 201
-            return make_response(response)
-
-        else:
-
-            response = {
-                'message': "You have no access token to use this resource"
-            }
-            return make_response(jsonify(response)), 401
+        response.status_code = 201
+        return make_response(response)
 
     @jwt_required
     def get(self, id=None):
 
-        current_user = get_jwt_identity()
-
-        if id and current_user:
+        if id:
 
             bucketlist = Bucketlist.query.filter_by(id=id).first()
 
@@ -59,8 +48,7 @@ class BucketlistAPI(MethodView):
 
                 response.status_code = 200
 
-        elif current_user:
-
+        else:
             bucketlists = Bucketlist.get_all()
             results = []
 
@@ -128,4 +116,47 @@ class BucketlistAPI(MethodView):
         return make_response(response)
 
 class BucketlistItemAPI(MethodView):
-    pass
+
+    """ Create Read Update Bucketlist items """
+
+    now = datetime.datetime.now()
+
+    @jwt_required
+    def post(self, id):
+        data = request.get_json()
+
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        bucketlist_item = BucketlistItem.query.filter_by(item_name=data.get('item_name')).first()
+
+        if not bucketlist_item:
+
+            new_item = BucketlistItem(item_name=data.get('item_name'), bucketlist=bucketlist.id,
+                                      done=data.get('done'), complete_by=data.get('complete_by'))
+            new_item.save()
+
+            response = jsonify({
+                'status': "Success",
+                'message': "Bucketlist Item Created"
+            })
+
+            response.status_code = 201
+
+        else:
+            response = jsonify({
+                'status': "Success",
+                'message': "Bucketlist Item Already Exists"
+            })
+
+            response.status_code = 200
+
+        return make_response(response)
+    #
+    #
+    # def get(self):
+    #     pass
+    #
+    # def put(self):
+    #     pass
+    #
+    # def delete(self):
+    #     pass
