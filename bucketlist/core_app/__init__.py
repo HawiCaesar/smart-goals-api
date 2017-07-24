80a0e3adb6226b6c12bf3fa382e3257c3ccc73a8
HawiCaesar <brian.odhiambo932@gmail.com>
2017-07-24 22:44:22 +0300

    [Feature 148479819] Add Routes for bucketlist items

--- bucketlist/core_app/__init__.py
+++ bucketlist/core_app/__init__.py
@@ -5,6 +5,7 @@ from . import views
 core_app = Blueprint('core_app', __name__)
 
 bucketlist_view = views.BucketlistAPI.as_view('bucketlist_api')
+bucketlist_item_view = views.BucketlistItemAPI.as_view('bucketlist_item_api')
 
 core_app.add_url_rule('/v1/api/bucketlists/',
                       view_func=bucketlist_view,
@@ -12,5 +13,12 @@ core_app.add_url_rule('/v1/api/bucketlists/',
 
 core_app.add_url_rule('/v1/api/bucketlists/<int:id>',
                       view_func=bucketlist_view,
-                      methods=['PUT', 'DELETE', 'GET']
-)
+                      methods=['PUT', 'DELETE', 'GET'])
+
+core_app.add_url_rule('/v1/api/bucketlists/<int:id>/',
+                      view_func=bucketlist_item_view,
+                      methods=['POST', 'GET'])
+
+core_app.add_url_rule('/v1/api/bucketlists/<int:id>/<int:item_id>',
+                      view_func=bucketlist_item_view,
+                      methods=['PUT', 'DELETE', 'GET'])
